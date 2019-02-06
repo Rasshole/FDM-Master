aa258287089617efd1375eac7902ff911beff64f
Rasshole <47320321+Rasshole@users.noreply.github.com>
2019-02-06 13:47:46 +0100

    v0.1

--- js/index.js
+++ js/index.js
@@ -114,5 +114,8 @@ function onDisconnect(){
 function onError(reason)  {
 	alert("ERROR: " + reason); // real apps should use notification.alert
 }
+function insertText() {
+	document.getElementById("aboutContent").innerHTML = "Fitness Digital Monitor helps you keep track of your fitness routines: How much weight you use, how many reps you take, how big your reps are and how long you take to complete your sets! This will help you keep track of your development as well as how much weight you used on previous workouts on the different machines. It is also great at motivating you, seeing your constant improvements";		
+	}
 
 	
